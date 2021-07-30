38a59944c5c56cf911c94eb00f8baea348f17ddf
ano201 <ahmu689@gmail.com>
2021-07-30 17:01:59 +0600

    a little advance (post)

--- liladv.js
+++ liladv.js
@@ -1,13 +1,19 @@
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-   title: 'Murad',
-   body: 'hey',
-   userId: 1,
-  }),
-  headers: {
-   'Content-type': 'application/json; charset=UTF-8',
-  },
- })
- .then((response) => response.json())
- .then((json) => console.log(json));
+const usrAction = document.getElementsByTagName("input");
+usrAction[2].addEventListener("click", function () {
+ const name = usrAction[0].value;
+ const email = usrAction[1].value;
+ const post = {name : name, email : email};
+ postToServer(post);
+})
+
+function postToServer(post) {
+ fetch('https://jsonplaceholder.typicode.com/posts', {
+   method: 'POST',
+   body: JSON.stringify(post),
+   headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+   },
+  })
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
